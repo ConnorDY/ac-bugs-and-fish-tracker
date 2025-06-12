ecbd876486d06fd73c8d6d35c89c2f491e1b9016
Connor Younglund <cdy@airmail.cc>
2025-06-11 18:27:44 -0700

    begin data entry for Doubutsu no Mori e+

---
+++ src/lib/data/doubutusu-no-mori-eplus.ts
@@ -0,0 +1,338 @@
+/**
+ * Doubutusu no Mori e+ (どうぶつの森 e+)
+ **/
+
+import {
+  Creature,
+  Location,
+  Month,
+  Shadow,
+  TimeOfDay,
+  type FishOrBug
+} from '$lib/types/fish-or-bug';
+
+export const fish: FishOrBug[] = [
+  // 1
+  {
+    name: 'Crucian Carp',
+    icon: 'crucian-carp.png',
+    type: Creature.FISH,
+    price: 120,
+    shadow: Shadow.SMALL,
+    location: [Location.RIVER],
+    time: TimeOfDay.ALL_DAY,
+    months: {
+      january: Month.ALL,
+      february: Month.ALL,
+      march: Month.ALL,
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL,
+      november: Month.ALL,
+      december: Month.ALL
+    }
+  },
+  // 2
+  {
+    name: 'Brook Trout',
+    icon: 'brook-trout.png',
+    type: Creature.FISH,
+    price: 150,
+    shadow: Shadow.MEDIUM,
+    location: [Location.LAKE],
+    time: TimeOfDay.ALL_DAY,
+    months: {
+      january: Month.ALL,
+      february: Month.ALL,
+      march: Month.ALL,
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL,
+      november: Month.ALL,
+      december: Month.ALL
+    }
+  },
+  // 3
+  {
+    name: 'Carp',
+    icon: 'carp.png',
+    type: Creature.FISH,
+    price: 300,
+    shadow: Shadow.LARGE,
+    location: [Location.RIVER],
+    time: TimeOfDay.ALL_DAY,
+    months: {
+      january: Month.ALL,
+      february: Month.ALL,
+      march: Month.ALL,
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL,
+      november: Month.ALL,
+      december: Month.ALL
+    }
+  },
+  // 4
+  {
+    name: 'Koi',
+    icon: 'koi.png',
+    type: Creature.FISH,
+    price: 2000,
+    shadow: Shadow.LARGE,
+    location: [Location.RIVER],
+    time: [
+      {
+        start: 16, // 4 PM
+        end: 9 // 9 AM
+      }
+    ],
+    months: {
+      january: Month.ALL,
+      february: Month.ALL,
+      march: Month.ALL,
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL,
+      november: Month.ALL,
+      december: Month.ALL
+    }
+  },
+  // 5
+  {
+    name: 'Barbel steed',
+    icon: 'barbel-steed.png',
+    type: Creature.FISH,
+    price: 200,
+    shadow: Shadow.LARGE,
+    location: [Location.RIVER],
+    time: TimeOfDay.ALL_DAY,
+    months: {
+      january: Month.ALL,
+      february: Month.ALL,
+      march: Month.ALL,
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL,
+      november: Month.ALL,
+      december: Month.ALL
+    }
+  },
+  // 6
+  {
+    name: 'Dace',
+    icon: 'dace.png',
+    type: Creature.FISH,
+    price: 200,
+    shadow: Shadow.MEDIUM,
+    location: [Location.RIVER],
+    time: [
+      {
+        start: 16, // 4 PM
+        end: 9 // 9 AM
+      }
+    ],
+    months: {
+      january: Month.ALL,
+      february: Month.ALL,
+      march: Month.ALL,
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL,
+      november: Month.ALL,
+      december: Month.ALL
+    }
+  },
+  // 7
+  {
+    name: 'Catfish',
+    icon: 'catfish.png',
+    type: Creature.FISH,
+    price: 200,
+    shadow: Shadow.LARGE,
+    location: [Location.RIVER],
+    time: [
+      {
+        start: 16, // 4 PM
+        end: 9 // 9 AM
+      }
+    ],
+    months: {
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL
+    }
+  },
+  // 8
+  {
+    name: 'Giant catfish',
+    icon: 'giant-catfish.png',
+    type: Creature.FISH,
+    price: 3000,
+    shadow: Shadow.VERY_LARGE,
+    location: [Location.LAKE],
+    time: [
+      {
+        start: 16, // 4 PM
+        end: 9 // 9 AM
+      }
+    ],
+    months: {
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL
+    }
+  },
+  // 9
+  {
+    name: 'Pale chub',
+    icon: 'pale-chub.png',
+    type: Creature.FISH,
+    price: 200,
+    shadow: Shadow.SMALL,
+    location: [Location.RIVER],
+    time: [
+      {
+        start: 9, // 9 AM
+        end: 16 // 4 PM
+      }
+    ],
+    months: {
+      january: Month.ALL,
+      february: Month.ALL,
+      march: Month.ALL,
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL,
+      november: Month.ALL,
+      december: Month.ALL
+    }
+  },
+  // 10
+  {
+    name: 'Bitterling',
+    icon: 'bitterling.png',
+    type: Creature.FISH,
+    price: 1300,
+    shadow: Shadow.TINY,
+    location: [Location.RIVER],
+    time: TimeOfDay.ALL_DAY,
+    months: {
+      january: Month.ALL,
+      february: Month.ALL,
+      december: Month.ALL
+    }
+  }
+];
+
+export const bugs: FishOrBug[] = [
+  // 1
+  {
+    name: 'Common Butterfly',
+    icon: 'common-butterfly.png',
+    type: Creature.BUG,
+    price: 80,
+    location: [Location.FLYING],
+    weather: {
+      noRain: true
+    },
+    time: [
+      {
+        start: 8, // 8 AM
+        end: 16 // 4 PM
+      }
+    ],
+    months: {
+      march: Month.ALL,
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL
+    }
+  },
+  // 2
+  {
+    name: 'Yellow Butterfly',
+    icon: 'yellow-butterfly.png',
+    type: Creature.BUG,
+    price: 80,
+    location: [Location.FLYING],
+    weather: {
+      noRain: true
+    },
+    time: [
+      {
+        start: 8, // 8 AM
+        end: 16 // 4 PM
+      }
+    ],
+    months: {
+      march: Month.ALL,
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL,
+      october: Month.ALL
+    }
+  },
+  // 3
+  {
+    name: 'Tiger butterfly',
+    icon: 'tiger-butterfly.png',
+    type: Creature.BUG,
+    price: 200,
+    location: [Location.FLYING],
+    weather: {
+      noRain: true
+    },
+    time: [
+      {
+        start: 8, // 8 AM
+        end: 16 // 4 PM
+      }
+    ],
+    months: {
+      april: Month.ALL,
+      may: Month.ALL,
+      june: Month.ALL,
+      july: Month.ALL,
+      august: Month.ALL,
+      september: Month.ALL
+    }
+  }
+];
